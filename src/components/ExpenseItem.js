1f5b1df51639f534eddad746c5ecd0dc33cb7340
Gaisensai <ajibabidunsin@gmail.com>
2021-12-25 15:36:55 +0100

    Tried using Normal React Syntax without JSX on the ExpenseItem Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,23 @@
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
+import React from 'react';
 
 function ExpenseItem(props) {
-    return (
+    // return React.createElement(Card,{className :"expense-item"},
+    //     React.createElement(ExpenseDate,{date:props.date}),
+    //     React.createElement('div',{className:"expense-item__description"},
+    //     React.createElement('h2',{},props.title),
+    //     React.createElement('div',{className:"expense-item__price"},props.amount))
+    // );
+     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-        </Card>
+               <h2>{props.title}</h2>
+               <div className='expense-item__price'>${props.amount}</div>
+           </div>
+       </Card>
     );
 }
 
